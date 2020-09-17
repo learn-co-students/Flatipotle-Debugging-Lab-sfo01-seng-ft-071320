e146e0ad6b2a206dfd1f730f4780444044276d75
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-16 20:52:28 -0700

    Done.

--- src/components/ProteinForm.js
+++ src/components/ProteinForm.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 
-const ProteinForm = () => {
+const ProteinForm = (props) => {
   return (
     <Fragment>
       <h1><label htmlFor="protein">Protein</label></h1>
@@ -83,5 +83,6 @@ const ProteinForm = () => {
     </Fragment>
   )
 }
-
 export default ProteinForm
+  
+
